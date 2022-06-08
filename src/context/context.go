216f5dfb9f349b37b9dc703e13886a46cce2067d
fuweihu.fwh <fuweihu.fwh@bytedance.com>
2022-06-09 00:52:22 +0800

    .

--- src/context/context.go
+++ src/context/context.go
@@ -201,6 +201,7 @@ var (
 	todo       = new(emptyCtx)
 )
 
+// 上下文的默认值，所有其他的上下文都应该从它衍生（Derived）出来
 // Background returns a non-nil, empty Context. It is never canceled, has no
 // values, and has no deadline. It is typically used by the main function,
 // initialization, and tests, and as the top-level Context for incoming
@@ -209,6 +210,7 @@ func Background() Context {
 	return background
 }
 
+// 应该只在不确定应该使用哪种上下文时使用
 // TODO returns a non-nil, empty Context. Code should use context.TODO when
 // it's unclear which Context to use or it is not yet available (because the
 // surrounding function has not yet been extended to accept a Context
